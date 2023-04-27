c72061fa9103df6b92fa772ba535545ca662ed4f
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-04-27 16:01:39 +0600

    topbar changed, from here I'm adding this repo to another profile. will come back later

--- src/shared/TopBar/TopBar.js
+++ src/shared/TopBar/TopBar.js
@@ -1,33 +1,35 @@
-import React from 'react'
-import logo from "../../assets/images/Admin/logo.png"
-import avatar from "../../assets/images/Admin/avatar.png"
+import React from "react";
+import logo from "../../assets/images/Admin/logo.png";
+import avatar from "../../assets/images/Admin/avatar.png";
 
-import { FaSearch } from 'react-icons/fa'
-import { FaCog } from 'react-icons/fa'
+import { FaSearch } from "react-icons/fa";
+import { FaCog } from "react-icons/fa";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const TopBar = () => {
-    return (
-        <div className='topbar py-3'>
-            <div className='px-8'>
-                <div className='flex justify-between items-center'>
-                    <img src={logo} alt='' />
-                    <div className='flex ml-auto justify-end items-center'>
-                        <a href='/'>
-                            <FaSearch className='ml-5 text-2xl' />
-                        </a>
-                        <Link to='/admin/settings'>
-                            <FaCog className='ml-5 text-2xl' />
-                        </Link>
-                        <Link to='/workers/dashboard'>
-                            <img src={avatar} className='ml-5' alt='' />
-                        </Link>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="topbar py-3">
+      <div className="px-8">
+        <div className="flex justify-between items-center">
+          <Link to="/admin/dashboard">
+            <img src={logo} alt="" />
+          </Link>
+          <div className="flex ml-auto justify-end items-center">
+            <a href="/">
+              <FaSearch className="ml-5 text-2xl" />
+            </a>
+            <Link to="/admin/settings">
+              <FaCog className="ml-5 text-2xl" />
+            </Link>
+            <Link to="/workers/dashboard">
+              <img src={avatar} className="ml-5" alt="" />
+            </Link>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TopBar
+export default TopBar;
